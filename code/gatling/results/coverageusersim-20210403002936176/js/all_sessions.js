ad57b94dd66a4e8848b10f5f911a77393b56ef75
abhisheksundarraman <abhishek.sundarraman@gmail.com>
2021-04-02 17:52:50 -0700

    Added the changes for the Coverage test for the Gatling

---
+++ code/gatling/results/coverageusersim-20210403002936176/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FF9D00',
+name: 'Active Users',
+data: [
+  [1617409778000,1],[1617409779000,1],[1617409780000,1],[1617409781000,1],[1617409782000,1],[1617409783000,1],[1617409784000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
